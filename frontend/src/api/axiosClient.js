975cf7001e046268808c49a7b085cb2b6fbaaf4f
rakiiith <trakith@gmail.com>
2026-04-27 20:13:00 +0530

    point legacy axios import to shared client

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,9 +1,1 @@
-import axios from 'axios';
-
-export const api = axios.create({
-    baseURL: 'http://localhost:8087/api/v1',
-    headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Basic ' + btoa('admin:1234')
-    }
-});
+export { api } from "./client";
